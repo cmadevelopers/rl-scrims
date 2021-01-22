1b5af63d72c03d79c06ef6df4f8fd9409971231e
Johnata Davi <infojohnata@gmail.com>
2021-01-22 15:18:37 -0300

    Update baseurl

--- front-end/src/services/config.js
+++ front-end/src/services/config.js
@@ -4,5 +4,6 @@ import axios from 'axios'
 // https://api.yannismate.de/rank/steam/RafaCampz?playlists=ranked_3v3,ranked_2v2
 
 export const http = axios.create({
-    baseURL: 'https://localhost/laradev/rl-project-api/public/api/'
+    // baseURL: 'https://localhost/laradev/rl-project-api/public/api/'
+    baseURL: 'https://rl-scrims-api.herokuapp.com/api/'
 })
